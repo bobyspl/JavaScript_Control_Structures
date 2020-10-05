3b7ba97585d3427662c6a9976184048f29e54f64
bobyspl <39329629+bobyspl@users.noreply.github.com>
2020-10-05 22:19:55 +0300

    Update control-structures.js

--- control-structures.js
+++ control-structures.js
@@ -4,21 +4,12 @@
 for (let i = 1; i <= 20; i++) {
     console.log(i);
 }
-console.log('break');
-
-
-
 
 
 // 2. display in the console the odd numbers from 1 to 20
 for (let i = 1; i < 20; i += 2) {
     console.log(i);
 }
-
-console.log('break');
-
-
-
 
 
 
@@ -31,12 +22,6 @@ for (let i = 0; i < array.length; i++) {
 }
 console.log(sum);
 
-console.log('break');
-
-
-
-
-
 
 
 // 4. compute the maximum of the elements of an array and display it in the console 
@@ -44,6 +29,21 @@ console.log('break');
 const maxArray = [10, 857, 122, 10, 15, 4, 182, 0];
 const maxNr = maxArray.length
 console.log(maxNr);
+
+
+
+// 5. compute how many times a certain element appears in an array
+const array2 = [1, 2, 4, 3, 4, 1, 5, 6, 4, 7, 8, 9, 1, 10, 1];
+
+function countNumber(param1, param2) {
+    let counter = 0;
+    for (let i = 0; i < array2.length; i++) {
+        if (array2[i] === param1)
+            counter += 1;
+        }
+        console.log(`The number appears: ${counter} times.`);
+}
+countNumber(4, array2)
 
 
 
